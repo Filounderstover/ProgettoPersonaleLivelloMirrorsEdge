c70b92082928a5eb042f883b4cfeb81663be97bd
Filippo <filippo.marsano@fastwebnet.it>
2022-03-15 17:07:05 +0100

    Menu funzionante
    I pulsanti collegano a quello che c'è scritto

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayTest()
+    {
+        SceneManager.LoadScene("LevelTest");
+    }
+
+    public void PlayLevel()
+    {
+        SceneManager.LoadScene("LevelPrototype");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Sei uscito :c");
+        Application.Quit();
+    }
+}
